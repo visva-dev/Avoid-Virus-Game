267219cb1cee43d72c1381a03ce62d27dd2e59de
Visvaldas <visva.rapalis@gmail.com>
2020-12-12 20:25:02 +0200

    Add enemies

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ gameScene.preload = function(){
   // load images
   this.load.image('background', './assets/background.png');
   this.load.image('player', './assets/player.png');
+  this.load.image('enemy', './assets/dragon.png');
 };
  
 // called once after the preload ends
@@ -19,7 +20,13 @@ gameScene.create = function() {
   let gameW = this.sys.game.config.width;
   let gameH = this.sys.game.config.height;
   
-  let player = this.add.sprite(70, 180, 'player');
+  let player = this.add.sprite(40, 310, 'player');
+
+  // create an enemy
+  let enemy1 = this.add.sprite(150, 80, 'enemy');
+
+  //create a second enemy
+  let enemy2 = this.add.sprite(270, 80, 'enemy');
 };
 
 // Set the configuration of the game
