2f47a44c254c4053a64020dc03880a51275a1ca8
Visvaldas <visva.rapalis@gmail.com>
2020-12-13 20:30:50 +0200

    Add more enemies and loose action

--- js/game.js
+++ js/game.js
@@ -2,100 +2,140 @@
 let gameScene = new Phaser.Scene('Game');
 
 // initiate scene parameters
-gameScene.init = function(){
+gameScene.init = function () {
   // player speed
   this.playerSpeed = 3;
 
-    // enemy speed
-    this.enemyMinSpeed = 1;
-    this.enemyMaxSpeed = 4.5;
-    
-    // boundaries
-    this.enemyMinY = 80;
-    this.enemyMaxY = 280;
+  // enemy speed
+  this.enemyMinSpeed = 1;
+  this.enemyMaxSpeed = 4.5;
+
+  // boundaries
+  this.enemyMinY = 80;
+  this.enemyMaxY = 280;
 };
- 
+
 // load assets
-gameScene.preload = function(){
+gameScene.preload = function () {
   // load images
   this.load.image('background', './assets/background.png');
   this.load.image('player', './assets/player.png');
   this.load.image('enemy', './assets/dragon.png');
   this.load.image('goal', './assets/treasure.png');
 };
- 
+
 // called once after the preload ends
-gameScene.create = function(){
+gameScene.create = function () {
   // create bg sprite
   let bg = this.add.sprite(0, 0, 'background');
-  
- 
+
   // change the origin to the top-left corner
   bg.setOrigin(0, 0);
-  
+
   // create the player
   this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
-  
+
   // we are reducing the width by 50%, and we are doubling the height
   this.player.setScale(0.5);
-  
+
   // goal
-  this.goal = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'goal');
+  this.goal = this.add.sprite(
+    this.sys.game.config.width - 80,
+    this.sys.game.config.height / 2,
+    'goal'
+  );
   this.goal.setScale(0.6);
 
-  // enemy
-  this.enemy = this.add.sprite(120, this.sys.game.config.height / 2, 'enemy');
-  this.enemy.flipX = true;
-  this.enemy.setScale(0.6);
-  
-  // set enemy speed
-  let dir = Math.random() < 0.5 ? 1 : -1;
-  let speed = this.enemyMinSpeed + Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
-  this.enemy.speed = dir * speed;
+  // enemy group
+  this.enemies = this.add.group({
+    key: 'enemy',
+    repeat: 5,
+    setXY: {
+      x: 90,
+      y: 100,
+      stepX: 80,
+      stepY: 20,
+    },
+  });
+
+  //setting scale to all group elements
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.4, -0.4);
+
+  //set flipX, and speed
+  Phaser.Actions.Call(
+    this.enemies.getChildren(),
+    function (enemy) {
+      //flip enemy
+      enemy.flipX = true;
+
+      //set speed
+      let dir = Math.random() < 0.5 ? 1 : -1;
+      let speed =
+        this.enemyMinSpeed +
+        Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+      enemy.speed = dir * speed;
+    },
+    this
+  );
 };
 
 // this is called up to 60 times per second
-gameScene.update = function(){
-  
+gameScene.update = function () {
   // check for active input
-  if(this.input.activePointer.isDown) {
+  if (this.input.activePointer.isDown) {
     // player walks
     this.player.x += this.playerSpeed;
   }
-  
+
   // treasure overlap check
   let playerRect = this.player.getBounds();
   let treasureRect = this.goal.getBounds();
-  
-  if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
+
+  if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
     console.log('reached goal!');
-    
+
     // restart the Scene
     this.scene.restart();
     return;
   }
 
+  //get enemies
+  let enemies = this.enemies.getChildren();
+  let numEnemies = enemies.length;
+
+  for (let i = 0; i < numEnemies; i++) {
     // enemy movement
-    this.enemy.y += this.enemy.speed;
-  
+    enemies[i].y += enemies[i].speed;
+
     // check we haven't passed min or max Y
-    let conditionUp = this.enemy.speed < 0 && this.enemy.y <= this.enemyMinY;
-    let conditionDown = this.enemy.speed > 0 && this.enemy.y >= this.enemyMaxY;
-    
-    // if we passed the upper or lower limit, reverse 
-    if(conditionUp || conditionDown) {
-      this.enemy.speed *= -1;
+    let conditionUp = enemies[i].speed < 0 && enemies[i].y <= this.enemyMinY;
+    let conditionDown = enemies[i].speed > 0 && enemies[i].y >= this.enemyMaxY;
+
+    // if we passed the upper or lower limit, reverse
+    if (conditionUp || conditionDown) {
+      enemies[i].speed *= -1;
     }
-  
+
+    // check enemy overlap
+    let enemyRect = enemies[i].getBounds();
+
+    if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)) {
+      console.log('Game over!');
+
+      // restart the Scene
+      this.scene.restart();
+      return;
+    }
+  }
 };
- 
+
 // set the configuration of the game
 let config = {
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
   width: 640,
   height: 360,
-  scene: gameScene
+  scene: gameScene,
 };
- 
+
 // create a new game, pass the configuration
 let game = new Phaser.Game(config);
